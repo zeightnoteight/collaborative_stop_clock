39ea3b9b4a50120e48bc5eaed0811ba4c478fc1c
Srinivas Devaki <mr.eightnoteight@gmail.com>
2018-03-07 10:16:55 +0530

    meh

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_clock(clock_id):
     if current_time - start_time > total_time:
         return 'expired'
     seconds_diff = int(total_time - (current_time - start_time))
-    diff_text = '{seconds_diff // (60 * 60)} hours, {(seconds_diff - ((seconds_diff // (60 * 60)) * 60 * 60)) // 60} minutes, {seconds_diff - (seconds_diff // 60) * 60} seconds left';
+    diff_text = '{seconds_diff // (60 * 60)} hours, {(seconds_diff - ((seconds_diff // (60 * 60)) * 60 * 60)) // 60} minutes, {seconds_diff - (seconds_diff // 60) * 60} seconds left'
     html = f"""
     <div style="display: flex; justify-content: center; align-items: center; font-size: 75px; width: 95vw; height: 95vh;">
         {seconds_diff // (60 * 60)} hours, {(seconds_diff - ((seconds_diff // (60 * 60)) * 60 * 60)) // 60} minutes, {seconds_diff - (seconds_diff // 60) * 60} seconds left
@@ -35,10 +35,10 @@ def get_clock(clock_id):
 @app.route('/')
 def main():
     clock_id = uuid.uuid4().hex
-    print(request.args.get('total_time'))
+    print(request.args.get('total_time', 600)) # 10 mins
     timers[clock_id] = {
         'start_time': time.time(),
-        'total_time': request.args.get('total_time')
+        'total_time': request.args.get('total_time', 600)
     };
     return redirect(url_for('.get_clock', clock_id=clock_id))
 
